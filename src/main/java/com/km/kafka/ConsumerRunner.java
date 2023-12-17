9231e968eb31defdcc8b41f6f09c6db98bc4a524
Kacper Marczewski <kacper.marczewski@gmail.com>
2023-12-17 18:00:26 +0100

    added producer and logging

--- src/main/java/com/km/kafka/ConsumerRunner.java
+++ src/main/java/com/km/kafka/ConsumerRunner.java
@@ -45,7 +45,7 @@ public class ConsumerRunner extends KafkaRunner {
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfiguration.getServer());
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfiguration.getConsumer());
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return properties;
